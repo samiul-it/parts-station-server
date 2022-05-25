1667928a22168e3dee6422252d667112af3ff682
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-05-25 11:21:18 +0600

    Storing Users in DB

--- index.js
+++ index.js
@@ -25,62 +25,87 @@ async function run() {
     await client.connect();
     const productCollection = client.db("parts-station").collection("products");
     const orderCollection = client.db("parts-station").collection("orders");
-    const userCollection=client.db("parts-station").collection("users");
+    const userCollection = client.db("parts-station").collection("users");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    //Posing Orders
+
+    app.post("/orders", async (req, res) => {
+      const newOrder = req.body;
+      const result = await orderCollection.insertOne(newOrder);
+      res.send(result);
+    });
+
+    //My Orders
+
+    app.get("/myorders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    });
+
+    //Reviews
+
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const reviews = await userCollection.find(query).toArray();
+      res.send(reviews);
+    });
+
+    //My Reviews
+
+    app.get("/reviews/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const myreview = await userCollection.find(query).toArray();
+      res.send(myreview);
+    });
+
+    //Add User
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
 
-     app.get("/products", async (req, res) => {
-       const query = {};
-       const cursor = productCollection.find(query);
-       const products = await cursor.toArray();
-       res.send(products);
-     });
 
+    //Add Review
 
-     app.get("/products/:id" ,async (req,res)=>{
-       const id=req.params.id;
-       const query={_id:ObjectId(id)};
-       const product=await productCollection.findOne(query);
-       res.send(product);
-     })
-
-     //Posing Orders
-
-     app.post("/orders",async (req,res)=>{
-       const newOrder=req.body;
-       const result=await  orderCollection.insertOne(newOrder);
-       res.send(result);
-     })
-
-     //My Orders
-
-     app.get("/myorders/:email",async (req,res)=>{
-       const email=req.params.email;
-       const query={userEmail:email};
-       const orders=await orderCollection.find(query).toArray();
-       res.send(orders);
-     })
-
-     //Reviews 
-
-     app.get("/reviews",async (req,res)=>{
-       const query={};
-       const reviews=await userCollection.find(query).toArray();
-       res.send(reviews);
-     })
-
-     //My Reviews
-
-     app.get("/reviews/:email", async (req, res) => {
-       const email=req.params.email;
-       const query = {email:email};
-       const myreview = await userCollection.find(query).toArray();
-       res.send(myreview);
-     });
-
-
-
-
-
+    app.put("/reviews/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          review:user.review,
+          rating:user.rating,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
 
 
