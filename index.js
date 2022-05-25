342d88a2bd4db43c0e2e9c45e209ccc1e27b09c1
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-05-25 14:18:24 +0600

    Verify Admin

--- index.js
+++ index.js
@@ -109,6 +109,16 @@ async function run() {
       res.send(result);
     });
 
+    //isAdmin?
+
+    app.get("/admin/:email",async (req,res)=>{
+      const email=req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+
+      res.send({ admin: isAdmin });
+    })
+
 
 
     //Add Review
