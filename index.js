f2e0e556ed7929f78bf4608eaf4a3ecfa78109ad
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-05-26 19:20:31 +0600

    Bugs Fixed

--- index.js
+++ index.js
@@ -201,6 +201,7 @@ async function run() {
       const email = req.params.email;
       const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === "admin";
+      console.log(isAdmin);
 
       res.send({ admin: isAdmin });
     });
